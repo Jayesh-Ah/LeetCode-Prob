fa772b41f70efafb47709eb11a65724037f64649
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-09-21 20:42:16 +0530

    Time: 56 ms (84.60%) | Memory: 12.2 MB (22.79%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,13 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        map = {}
-        l = 0
+        char_count = {}
+        left = 0
+        max_count = 0
         max_len = 0
-        for r in range(len(s)):
-            map[s[r]] = 1 + map.get(s[r], 0)
-            if r-l+1 - max(map.values()) > k:
-                map[s[l]] -= 1
-                l+= 1
-            max_len = max(max_len, r-l+1)
+        
+        for right in range(len(s)):
+            # Update the count for the current character
+            char_count[s[right]] = 1 + char_count.get(s[right], 0)
+            
+            # Track the maximum frequency of any character in the current window
+            max_count = max(max_count, char_count[s[right]])
+            
+            # If the number of characters to be replaced exceeds 'k', shrink the window
+            if (right - left + 1) - max_count > k:
+                char_count[s[left]] -= 1
+                left += 1
+            
+            # Update the maximum length of the valid window
+            max_len = max(max_len, right - left + 1)
+        
         return max_len
