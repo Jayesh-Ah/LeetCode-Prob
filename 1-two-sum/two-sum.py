ff426ac1c02cda5cd63bbb3e0dadec7e9022d225
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-09-21 19:44:03 +0530

    Time: 70 ms (53.74%) | Memory: 13.1 MB (5.08%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,9 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        count = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in count:
-                return [count[diff], i]
-            count[nums[i]] = i
+        indexed_nums = [(num, i) for i, num in enumerate(nums)]
+        indexed_nums.sort(key=lambda x: x[0])
+        i, j = 0, len(nums)-1 
+        while i < j:
+            if indexed_nums[i][0] + indexed_nums[j][0] == target:
+                return[indexed_nums[i][1], indexed_nums[j][1]]
+            elif indexed_nums[i][0] + indexed_nums[j][0] < target:
+                i+=1
+            else:
+                j-=1
